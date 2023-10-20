aa265565797492b97b1df3d946760e0fb960c96a
DiogoSV7 <up202208723@up.pt>
2023-10-20 22:19:27 +0100

    Merged Uc and UcClasses into UcClass and deleted Uc.h and Uc.cpp

--- src/classes/UcClass.cpp
+++ src/classes/UcClass.cpp
@@ -1,12 +1,38 @@
 #include "UcClass.h"
-#include <string>
-using namespace std;
 
-UcClass::UcClass(string dayOfTheWeek, float startTime, string classType, float classDuration) {
+UcClass::UcClass(string ucCode, string codeOfClass, string dayOfTheWeek, float startTime, string classType, float classDuration) {
+    this->ucCode_ = ucCode;
+    this->codeOfClass_ = codeOfClass;
     this->dayOfTheWeek_ = dayOfTheWeek;
     this->startTime_ = startTime;
     this->classType_ = classType;
     this->classDuration_ = classDuration;
+    this->numberOfStudents_ = 0;
+    this->maxCapacity_ = 0;
+}
+
+string UcClass::get_ucCode() const {
+    return ucCode_;
+}
+
+string UcClass::get_codeOfClass() const {
+    return codeOfClass_;
+}
+
+int UcClass::get_numberOfStudents() const {
+    return numberOfStudents_;
+}
+
+int UcClass::get_maxCapacity() const {
+    return maxCapacity_;
+}
+
+void UcClass::set_maxCapacity(int newMaxCapacity) {
+    maxCapacity_ = newMaxCapacity;
+}
+
+void UcClass::set_numberOfStudents(int newNumberOfStudents) {
+    numberOfStudents_ = newNumberOfStudents;
 }
 
 string UcClass::get_dayOfTheWeek() const {
@@ -17,7 +43,7 @@ float UcClass::get_startTime() const {
     return this->startTime_;
 }
 
-string UcClass::get_classType() const{
+string UcClass::get_classType() const {
     return this->classType_;
 }
 
@@ -29,11 +55,10 @@ bool UcClass::Overlaps(UcClass lesson) const {
     if (this->dayOfTheWeek_ == lesson.dayOfTheWeek_) {
         float thisEnd = this->startTime_ + this->classDuration_;
         float lessonEnd = lesson.startTime_ + lesson.classDuration_;
-        if (this->startTime_ <= lessonEnd && thisEnd >= lesson.startTime_) {
+        if (this->startTisme_ <= lessonEnd && thisEnd >= lesson.startTime_) {
             return true;
         }
     }
     return false;
 }
-
 
--- src/classes/UcClass.h
+++ src/classes/UcClass.h
@@ -1,68 +1,112 @@
 #ifndef UCCLASS_H
 #define UCCLASS_H
 
-#include <fstream>
-#include <sstream>
-#include <iostream>
+
+#include <vector>
 #include <string>
 using namespace std;
 
 class UcClass {
-    public:
-        /**
-         * @brief UcClass parameterized constructor
-         * @param dayOfTheWeek
-         * @param startTime
-         * @param classType
-         * @param classDuration
-         * @author Diogo Vieira
-         */
-        UcClass(string dayOfTheWeek, float startTime, string classType, float classDuration);
+public:
+    /**
+     * Parameterized constructor for UcClass
+     * @param ucCode
+     * @param codeOfClass
+     * @param dayOfTheWeek
+     * @param startTime
+     * @param classType
+     * @param classDuration
+     * @author Diogo Vieira
+     */
+    UcClass(string ucCode, string codeOfClass, string dayOfTheWeek, float startTime, string classType, float classDuration);
 
-        /**
-         * @brief dayOfTheWeek getter
-         * @return dayOfTheWeek
-         * @author Diogo Vieira
-         */
-        string get_dayOfTheWeek() const;
+    /**
+     * @brief ucCode getter
+     * @return ucCode
+     * @author Diogo Vieira
+     */
+    string get_ucCode() const;
 
-        /**
-         * @brief classType getter
-         * @return classType
-         * @author Diogo Vieira
-         */
-        string get_classType() const;
+    /**
+     * @brief codeOfClass getter
+     * @return codeOfClass
+     * @author Diogo Vieira
+     */
+    string get_codeOfClass() const;
 
-        /**
-         * @brief startTime getter
-         * @return startTime
-         * @author Diogo Vieira
-         */
-        float get_startTime() const;
+    /**
+     * @brief dayOfTheWeek getter
+     * @return dayOfTheWeek
+     * @author Diogo Vieira
+     */
+    string get_dayOfTheWeek() const;
 
-        /**
-         * @brief classDuration getter
-         * @return classDuration
-         * @author Diogo Vieira
-         */
-        float get_classDuration() const;
+    /**
+     * @brief classType getter
+     * @return classType
+     * @author Diogo Vieira
+     */
+    string get_classType() const;
 
-        /**
-         * @brief Checks if two lessons overlap
-         * @param lesson
-         * @return true if two lessons overlaps, false if they don´t
-         * @author Diogo Vieira
-         */
-        bool Overlaps(UcClass lesson) const;
+    /**
+     * @brief startTime getter
+     * @return startTime
+     * @author Diogo Vieira
+     */
+    float get_startTime() const;
 
-    public:
-        string dayOfTheWeek_;
-        float startTime_;
-        string classType_;
-        float classDuration_;
+    /**
+     * @brief classDuration getter
+     * @return classDuration
+     * @author Diogo Vieira
+     */
+    float get_classDuration() const;
+
+    /**
+     * @brief numberOfStudents getter
+     * @return numberOfStudents
+     * @author Diogo Vieira
+     */
+    int get_numberOfStudents() const;
+
+    /**
+     * @brief maxCapacity getter
+     * @return maxCapacity
+     * @author Diogo Vieira
+     */
+    int get_maxCapacity() const;
+
+    /**
+     * @brief Checks if two lessons overlap
+     * @param lesson
+     * @return true if two lessons overlap, false if they don't
+     * @author Diogo Vieira
+     */
+    bool Overlaps(UcClass lesson) const;
+
+    /**
+     * @brief numberOfStudents establishment
+     * @param newNumberOfStudents
+     * @author Diogo Vieira
+     */
+    void set_numberOfStudents(int newNumberOfStudents);
+
+    /**
+     * @brief maxCapacity establishment
+     * @param newMaxCapacity
+     * @author Diogo Vieira
+     */
+    void set_maxCapacity(int newMaxCapacity);
+
+private:
+    string ucCode_;
+    string codeOfClass_;
+    string dayOfTheWeek_;
+    float startTime_;
+    string classType_;
+    float classDuration_;
+    int numberOfStudents_;
+    int maxCapacity_;
 };
-
-
-
 
 #endif
