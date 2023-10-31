a08832b7ff9adb7e5db3ced8e199cdc6b0bddc80
The-Memechanic <jpmmendes04@gmail.com>
2023-10-31 10:41:08 +0000

    Merge fix

--- src/classes/UcClass.cpp
+++ src/classes/UcClass.cpp
@@ -21,12 +21,6 @@ UcClass::UcClass(const string& uc_code, const string& class_code, const vector<L
     this->number_of_students_ = 0;
 }
 
-UcClass::UcClass(const string& uc_code, const string& class_code) {
-    this->uc_code_ = uc_code;
-    this->class_code_ = class_code;
-    this->number_of_students_ = 0;
-}
-
 string UcClass::getUcCode() const {
     return uc_code_;
 }
