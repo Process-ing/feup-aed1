2be4cf4295ce116c5e1da27ea7e1751136fce03b
The-Memechanic <jpmmendes04@gmail.com>
2023-10-31 10:29:19 +0000

    Request add logic

--- src/classes/Request.cpp
+++ src/classes/Request.cpp
@@ -1,4 +1,5 @@
 #include "Request.h"
+#include <iostream>
 
 Request::Request(Request::Type type, Student& student, UcClass& target_class) {
     type_ = type;
@@ -9,7 +10,38 @@ Request::Request(Request::Type type, Student& student, UcClass& target_class) {
 void Request::perform(Dataset &dataset) {
     switch (type_) {
         case ADD:
-            //TODO
+            for (UcClass* const& c : student_->getUcClasses()) {
+                if (c->getUcCode() == target_class_->getUcCode()) {
+                    std::cout << "Request failed: already in this UC" << std::endl;
+                    break;
+                }
+            }
+            //if (target_class_->getNumberOfStudents() == max) {
+            //    std::cout << "Request failed: class full" << std::endl;
+            //    break;
+            //}
+            for (UcClass* const& c : student_->getUcClasses()) {
+                if (c->getNumberOfStudents() - target_class_->getNumberOfStudents() == 4
+                 || c->getNumberOfStudents() - target_class_->getNumberOfStudents() == -4) {
+                    std::cout << "Request failed: entering this class would disturb class occupation balance" << std::endl;
+                    break;
+                }
+            }
+            for (UcClass* const& c : student_->getUcClasses()) {
+                for (Lesson const& l : c->getLessons()) {
+                    if (l.getType() == Lesson::TP || l.getType() == Lesson::PL) {
+                        for (Lesson const &tl: target_class_->getLessons()) {
+                            if (tl.getType() == Lesson::TP || tl.getType() == Lesson::PL) {
+                                if (l.overlaps(tl)) {
+                                    std::cout << "Request failed: target class's lessons overlap with your schedule" << std::endl;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            student_->getUcClasses().insert(student_->getUcClasses().end(), target_class_);
             break;
         case REMOVE:
             //TODO
